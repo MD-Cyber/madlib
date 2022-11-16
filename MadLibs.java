1c127c8ee1adf138a25a1bb6e37df36f869894e7
md-cyber <>
2022-11-16 14:04:41 +0530

    initial commit

---
+++ MadLibs.java
@@ -0,0 +1,11 @@
+public class MadLibs {
+
+    /* This program generates a mad libbed story.
+       Author: 
+       Date: 15/11/2022
+     */
+
+    public static void main(String[] args){
+
+    }
+}
